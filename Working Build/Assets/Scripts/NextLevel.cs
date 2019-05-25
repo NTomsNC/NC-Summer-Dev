e068249afa90cac0ef2c8e448f910ac9f5d24b75
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-05-25 16:00:49 -0400

    Clean version of the final project
    Created a clean and working version of a final project, this can be used to implement menus and finalized features
    
    Also make the level builder a singleton

---
+++ Working Build/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public LevelBuilder levelBuilder;
+    int level;
+
+    private void Start()
+    {
+        levelBuilder = GameObject.FindGameObjectWithTag("LevelBuilder").GetComponent<LevelBuilder>();
+        level = levelBuilder.Level;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerPrefs.SetInt("Level", level + 1);
+            SceneManager.LoadScene(0);
+        }
+    }
+}
