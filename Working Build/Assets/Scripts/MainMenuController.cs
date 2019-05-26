a5808d0466132f6aaf7baf0b0f5d21a1d64df8cd
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-05-26 14:39:55 -0400

    First Framework for Main Menu UI
    Main menu, options save canvases were added to the project, all save states load the level builder, therefore saves do not yet work

---
+++ Working Build/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Menu States")]
+    public GameObject mainMenuCanvas;
+    public GameObject playerSavesCanvas;
+    public GameObject SettingsCanvas;
+
+    public enum MenuState { Main, Saves, Options};
+    public MenuState menuState;
+
+    public GameObject levelController;
+
+    // Singleton (Awake)
+    #region Singleton Pattern
+    public static MainMenuController Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        mainMenuCanvas.SetActive(true);
+        SettingsCanvas.SetActive(false);
+        playerSavesCanvas.SetActive(false);
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for the menu state of Options
+        if (menuState == MenuState.Options) {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menuState = MenuState.Main;
+
+                mainMenuCanvas.SetActive(true);
+                SettingsCanvas.SetActive(false);
+            }
+        }
+
+        // Check for the menu state of Saves
+        if (menuState == MenuState.Saves)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menuState = MenuState.Main;
+
+                mainMenuCanvas.SetActive(true);
+                playerSavesCanvas.SetActive(false);
+            }
+        }
+    }
+
+    public void HandleOptionsBtn()
+    {
+        menuState = MenuState.Options;
+        mainMenuCanvas.SetActive(false);
+        SettingsCanvas.SetActive(true);
+    }
+
+    public void HandleSavesBtn()
+    {
+        menuState = MenuState.Saves;
+        mainMenuCanvas.SetActive(false);
+        playerSavesCanvas.SetActive(true);
+    }
+
+    public void LoadGame()
+    {
+        GameObject go = GameObject.Instantiate(levelController);
+        playerSavesCanvas.SetActive(false);
+    }
+}
