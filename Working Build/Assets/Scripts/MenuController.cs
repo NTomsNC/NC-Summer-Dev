be56ffcfd4876601723e09c06f5c1e86bccf3e1a
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-06-01 12:10:05 -0400

    (WIP) Added a button to return to the main menu, Also changing the cript to Main Menu controller to Menu Controller

---
+++ Working Build/Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //[Header("Menu States")]
+    //public GameObject mainMenuCanvas;
+    //public GameObject playerSavesCanvas;
+    //public GameObject SettingsCanvas;
+
+    public enum MenuState { Main, Saves, Options };
+    public MenuState menuState;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene_MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadScene_Game(int saveNum)
+    {
+        LoadPlayerData(saveNum);
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadPlayerData(int saveNum)
+    {
+        PlayerPrefs.SetInt("CurrentSave", saveNum);
+        PlayerPrefs.Save();
+    }
+}
