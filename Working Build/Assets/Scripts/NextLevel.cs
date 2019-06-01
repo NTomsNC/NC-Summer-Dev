7001ab7c0552cb8ae46be6953ef7796f515c6fb5
houghtob123 <houghtob123@gmail.com>
2019-05-31 22:42:39 -0400

    revert 2
    used to get back progress on level builder

--- Working Build/Assets/Scripts/NextLevel.cs
+++ Working Build/Assets/Scripts/NextLevel.cs
@@ -16,9 +16,18 @@ public class NextLevel : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+<<<<<<< HEAD
             PlayerPrefs.SetInt("Level", level + 1);
             PlayerPrefs.SetInt("Seed", System.DateTime.Now.Second);
             PlayerPrefs.Save();
+=======
+            saveGame = GameObject.FindGameObjectWithTag("LevelBuilder").GetComponent<LevelBuilder>().saveGame;
+            saveNum = GameObject.FindGameObjectWithTag("LevelBuilder").GetComponent<LevelBuilder>().saveNum;
+            Transform pTransform = new GameObject().transform;
+            pTransform.position = new Vector3(0,1,0);
+
+            saveGame.Save(saveGame.level + 1, System.DateTime.Now.Second, pTransform, saveNum);
+>>>>>>> parent of 0db3f975... DateTime.Now.Seconds changed to DateTime.UTCTime.ticks
 
             SceneManager.LoadScene(0);
         }
