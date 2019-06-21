d66a19577688a200a23435a81fd8e97213485404
houghtob123 <houghtob123@gmail.com>
2019-06-20 22:14:42 -0400

    Started a somewhat shitty visibility system & LWRP
    -Multiple scripts and a shader to allow objects to fade based on distance to sphere (need to change to distance from ray)
    -Converted to LWRP for simpler fade shader creation

---
+++ Working Build/Assets/Scripts/Dissolver/DissolveDistance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------------------------
+//Too be used with VisibilitySphere script and Dissolve material
+public class DissolveDistance : MonoBehaviour
+{
+    [Tooltip("The distance in meters that material will start to dissolve.")]
+    public float startDissolveDistance = 1;
+
+    private GameObject sphere;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+        rend.material.shader = Shader.Find("Custom/Dissolve");
+        rend.material.SetFloat("_Dissolve", 1);
+
+        sphere = GameObject.FindGameObjectWithTag("VisibilitySphere");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphere = GameObject.FindGameObjectWithTag("VisibilitySphere");
+
+        if (sphere != null)
+        {            
+            Vector3 p1 = sphere.GetComponent<SphereCollider>().bounds.ClosestPoint(transform.position);
+            Vector3 p2 = GetComponent<MeshCollider>().bounds.ClosestPoint(sphere.transform.position);
+
+            float distance = Vector3.Distance(p1, p2);
+
+            {
+                float dissolve = distance / startDissolveDistance;
+                if (dissolve > 1) dissolve = 1;
+
+                rend.material.SetFloat("_Dissolve", dissolve);
+            }
+        }
+    }
+}
---
+++ Working Build/Assets/Scripts/Dissolver/VisibilitySphere.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilitySphere : MonoBehaviour
+{
+    public Vector3 collisionCenter = new Vector3();
+    public LayerMask layers;
+    public float radius = 0.1f;
+    public GameObject spherePrefab;
+    public float speed = 50;
+
+    private GameObject sphere;
+    private bool debugTriggered = false;
+
+    //for use outside script
+    public GameObject Sphere
+    {
+        get { return sphere; }
+    }
+
+    // FixedUpdate is called once per physics frame
+    void FixedUpdate()
+    {
+        DetermineCameraVisibility();
+        DrawSphere();
+    }
+
+    private void DetermineCameraVisibility()
+    {
+        Vector3 offset = new Vector3(0, 0.5f, 0);
+        Ray ray = new Ray(transform.position + offset, Camera.main.transform.position - transform.position);
+
+        RaycastHit hitInfo = new RaycastHit();
+
+        Debug.DrawRay(transform.position + offset, Camera.main.transform.position - transform.position);
+        if (Physics.SphereCast( ray, radius, out hitInfo))
+        {
+            //Sets where the ray hit for dissolver code
+            collisionCenter = hitInfo.point;
+            //collisionCenter.y = 0.5f;
+        }
+        else
+        {
+            //Sets where ray hit to ungodly height
+            collisionCenter = Camera.main.transform.position + new Vector3(0, 15, 0);
+        }
+    }
+
+    private void DrawSphere()
+    {
+        if(spherePrefab != null)
+        {
+            if(sphere == null)
+            {
+                sphere = Instantiate(spherePrefab, collisionCenter, Quaternion.identity);
+                sphere.tag = "VisibilitySphere";
+            }
+            else
+            {
+                //sphere.transform.position = position;
+                sphere.transform.position = Vector3.MoveTowards(sphere.transform.position, collisionCenter, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if(!debugTriggered)
+            {
+                Debug.LogError("Sphere Prefab Missing. Please input prefab.");
+            }
+        }
+    }
+}
